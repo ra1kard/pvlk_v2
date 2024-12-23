1b52936d08db95b47e98e2d2e1e983f459165b4a
ra1kard_air <91raykard@gmail.com>
2024-12-23 19:57:26 +0300

    task12

---
+++ src/main/java/org/interfaces_functional/task12/IpAddress.java
@@ -0,0 +1,14 @@
+package org.interfaces_functional.task12;
+
+public class IpAddress {
+    private String address;
+
+    public IpAddress(String ipAddress) {
+        this.address = ipAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+}
---
+++ src/main/java/org/interfaces_functional/task12/Start.java
@@ -0,0 +1,30 @@
+package org.interfaces_functional.task12;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+
+/*public class Start {
+
+    public static void main(String[] args) {
+
+        TreeSet<IpAddress> list = new TreeSet<>(List.of(
+                new IpAddress("192.168.0.1"),
+                new IpAddress("192.168.0.22"),
+                new IpAddress("192.168.10.15"),
+                new IpAddress("255.255.0.255"),
+                new IpAddress("0.0.0.0"),
+                new IpAddress("192.168.1.22")
+        ));
+
+        Comparator<IpAddress> ipAddressComparator = (o1, o2) -> {
+            if (o1.getAddress().compareTo(o2.getAddress()) > 0) return 1;
+            if (o1.getAddress().compareTo(o2.getAddress()) < 0) return -1;
+            return 0;
+        };
+
+        list.sort
+
+    }
+
+}*/
